40375128990f6484e64054b3932df34030263d92
XunOnline <steve.niedermeyer001@gmail.com>
2022-08-10 21:55:23 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,45 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-ctx.lineWidth = 10;
+ctx.lineWidth = 2;
 
 // Wall
-ctx.strokeRect(75, 140, 150, 110);
+ctx.strokeRect(35, 100, 150, 110);
 
 // Door
-ctx.fillRect(130, 190, 40, 60);
+ctx.strokeRect(55, 120, 30, 90);
+
+//window
+ctx.strokeRect(115, 110, 50, 30);
+ctx.strokeRect(140, 110, 25, 30);
+ctx.strokeRect(115, 125, 50, 15);
 
 // Roof
 ctx.beginPath();
-ctx.moveTo(120, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
+ctx.moveTo(10, 100);
+ctx.lineTo(110, 60);
+ctx.lineTo(210, 100);
+ctx.closePath();
+ctx.stroke();
+
+//window in roof
+ctx.beginPath();
+ctx.arc(110, 80, 12, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(110, 80, 8, 0, 2 * Math.PI);
+ctx.stroke();
+
+//sun
+ctx.beginPath();
+ctx.arc(5, 5, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+//doggo
+
+//rays
+ctx.beginPath();
+ctx.moveTo(5, 55);
+ctx.lineTo(8, 60);
 ctx.closePath();
 ctx.stroke();
